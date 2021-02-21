8d76b2dd0b9c424517e620c738ee4392fe121961
Chisty Md.Muzammel Hossain <chistyz10@gmail.com>
2021-02-21 23:30:22 +0600

    MiddlewareUpdate

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+use App\User;
+use Illuminate\Support\Facades\DB;
 class LoginController extends Controller
 {
     public function index(){
@@ -12,14 +13,25 @@ class LoginController extends Controller
 
     public function verify(Request $req){
 
-        if($req->username == "" || $req->password == ""){
+        
+        $user = User::where('password',$req->username)
+                    ->where('username',$req->username)
+                    ->get();
+
+        print_r($user);
+        
+        
+        /*if($req->username == "" || $req->password == ""){
             echo "null submission";
         }elseif($req->username == $req->password){
             $req->session()->put('username',$req->username);
             return redirect('/home');
-        }else{
+        }
+
+
+        else{
             $req->session()->flash('msg','invalid request login first');
             return redirect('/login');
-        }
+        }*/
     }
 }
